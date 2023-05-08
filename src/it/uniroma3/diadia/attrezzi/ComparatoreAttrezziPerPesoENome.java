de27664c3a9889e07e566ee21a922d414a390f0d
W3500 <85308375+W3500@users.noreply.github.com>
2023-05-08 11:40:35 +0200

    Modificato metodo di comparazione

--- src/it/uniroma3/diadia/attrezzi/ComparatoreAttrezziPerPesoENome.java
+++ src/it/uniroma3/diadia/attrezzi/ComparatoreAttrezziPerPesoENome.java
@@ -2,10 +2,12 @@ package it.uniroma3.diadia.attrezzi;
 
 import java.util.Comparator;
 
-public class ComparatoreAttrezziPerPeso implements Comparator<Attrezzo> {
+public class ComparatoreAttrezziPerPesoENome implements Comparator<Attrezzo> {
 
 	@Override
 	public int compare(Attrezzo o1, Attrezzo o2) {
+		if(o1.getPeso()-o2.getPeso()==0)
+			return o1.getNome().compareTo(o2.getNome());
 		return o1.getPeso()-o2.getPeso();
 	}
 
