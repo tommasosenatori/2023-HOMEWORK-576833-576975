200ddcf60c36d9604208ca411e4a85ed872700f1
W3500 <85308375+W3500@users.noreply.github.com>
2023-06-08 15:28:21 +0200

    Aggiunto classe con costanti di default

---
+++ src/it/uniroma3/diadia/Costanti.java
@@ -0,0 +1,8 @@
+package it.uniroma3.diadia;
+
+public class Costanti {
+	public static int CFU_INIZIALI = 20;
+	public static int NUMERO_MASSIMO_ATTREZZI = 10;
+	public static int DEFAULT_PESO_MAX_BORSA = 10; 
+
+}
